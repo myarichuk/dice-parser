86e5b6106df8052bb6bffe5387d233faf756802f
Michael Yarichuk <michael.yarichuk@gmail.com>
2022-06-05 14:36:48 +0300

    fix: reorganize project structure

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import {Dice, DiceExpression} from './types';
 export default class DiceExpr {
   private data: DiceExpression | Dice | number;
 
-  constructor(diceExpr: string) {
+  public constructor(diceExpr: string) {
     const parseResult = diceExprParser().parse(Streams.ofString(diceExpr));
     if (!parseResult.isAccepted()) {
       throw new Error(`failed to parse '${diceExpr}'`);
